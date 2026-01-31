de7595bfa2689fc3ed70b4611f2d4595dcde49d7
Rustam <rhashhanok2020@mail.ru>
2026-01-31 22:08:41 +0300

    Test---2

--- app.py
+++ app.py
@@ -513,6 +513,6 @@ if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "False").lower() == "true"
     
     logger.info(f"Запуск Flask веб-сервера на {host}:{port} (debug={debug})")
-    logger.info(f"Тестовый запуск")
+    logger.info(f"Тестовый запуск 2")
     app.run(host=host, port=port, debug=debug)
 
